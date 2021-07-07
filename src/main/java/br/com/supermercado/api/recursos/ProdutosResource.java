8acf0d1fa41028148e46fa44384a27cb25ef93d0
Daroz <geraldodarosfilho@hotmail.com>
2021-07-07 11:55:04 -0300

    Implementação dos endpoints dos verbos HTTP

--- src/main/java/br/com/supermercado/api/recursos/ProdutosResource.java
+++ src/main/java/br/com/supermercado/api/recursos/ProdutosResource.java
@@ -1,13 +1,38 @@
 package br.com.supermercado.api.recursos;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import br.com.supermercado.api.models.Produto;
+
+import javax.ws.rs.*;
 
 @Path("/produtos")
 public class ProdutosResource {
+
     @GET
-    public String HelloWorld(){
-        return "Hello World, Supermercado";
+    public String pegarTodosProdutos() {
+        return "Pegar todos os produtos";
+    }
+
+    @GET
+    @Path("/{idDoProduto}")
+    public String pegarUmProduto(@PathParam("idDoProduto") Long idDoProduto) {
+        return "Pegar produto, id do produto: " + idDoProduto;
+    }
+
+    @POST
+    public String criarUmProduto(Produto produto){
+        return "Produto criado, ID: " + produto.getNome();
+    }
+
+    @PUT
+    @Path("/{idDoProduto}")
+    public String atualizarUmProduto( @PathParam("idDoProduto") Long idDoProduto ,Produto produto){
+        return "Produto atualizado, ID: " + idDoProduto + " Nome do produto " + produto.getNome() + " com o valor de: R$" + produto.getPreco();
+    }
+
+    @DELETE
+    @Path("/{idDoProduto}")
+    public  String excluirUmProduto(@PathParam("idDoProduto") Long idDoProduto){
+        return "Produto excluido! ID: " + idDoProduto;
     }
 
 }
